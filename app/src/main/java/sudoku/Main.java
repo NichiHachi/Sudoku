0014f3b435f53766843fae5a80c605f6d519e98c
Xeon0X <xeon0x.gaming@gmail.com>
2025-01-30 16:41:43 +0100

    fix(main): clean main

--- app/src/main/java/sudoku/Main.java
+++ app/src/main/java/sudoku/Main.java
@@ -1,21 +1,15 @@
 package sudoku;
 
-import solvers.wfc.WaveFunctionCollapse;
-
 import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        Sudoku sudoku1 = new Sudoku(new String[]{"1", "2", "3", "4"});
-        Sudoku sudoku2 = new Sudoku(new String[]{"1", "2", "3", "4"}, 1);
+        Sudoku sudoku0 = new Sudoku(new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9"});
 
         ArrayList<Sudoku> sudokus = new ArrayList<>();
-        sudokus.add(sudoku1);
-//        sudokus.add(sudoku2);
-//        sudokus.add(sudoku3);
+        sudokus.add(sudoku0);
 
         Grid grid = new Grid(sudokus);
-        WaveFunctionCollapse solver = new WaveFunctionCollapse(grid);
-        solver.solve();
+        grid.playTerminal();
     }
 }
