576f7f2dd6389ef2b640261ca6218c57bc545be1
Xeon0X <xeon0x.gaming@gmail.com>
2025-01-30 16:46:23 +0100

    feat(backtrack): add backtrack

--- app/src/main/java/sudoku/Main.java
+++ app/src/main/java/sudoku/Main.java
@@ -6,16 +6,19 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        Sudoku sudoku1 = new Sudoku(new String[]{"1", "2", "3", "4"});
-        Sudoku sudoku2 = new Sudoku(new String[]{"1", "2", "3", "4"}, 1);
+        Sudoku sudoku0 = new Sudoku(new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9"});
+        // Sudoku sudoku1 = new Sudoku(new String[]{"1", "2", "3", "4"});
+        // Sudoku sudoku2 = new Sudoku(new String[]{"1", "2", "3", "4"}, 1);
 
         ArrayList<Sudoku> sudokus = new ArrayList<>();
-        sudokus.add(sudoku1);
-//        sudokus.add(sudoku2);
-//        sudokus.add(sudoku3);
+        sudokus.add(sudoku0);
+        // sudokus.add(sudoku1);
+        //sudokus.add(sudoku2);
+        // sudokus.add(sudoku3);
 
         Grid grid = new Grid(sudokus);
-        WaveFunctionCollapse solver = new WaveFunctionCollapse(grid);
-        solver.solve();
+        // WaveFunctionCollapse solver = new WaveFunctionCollapse(grid);
+        // solver.solve();
+        grid.playTerminal();
     }
 }
