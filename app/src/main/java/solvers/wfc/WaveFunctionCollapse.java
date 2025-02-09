34dee6abfe657dbefa82ba9bc85cf22daefe6826
Xeon0X <xeon0x.gaming@gmail.com>
2025-02-09 19:33:34 +0100

    docs(wtc): add documentation for wtc

--- app/src/main/java/solvers/wfc/WaveFunctionCollapse.java
+++ app/src/main/java/solvers/wfc/WaveFunctionCollapse.java
@@ -12,6 +12,15 @@ import sudoku.Position;
 import sudoku.rule.Rule;
 import utils.Colors;
 
+/**
+ * Implements the Wave Function Collapse algorithm to solve Sudoku puzzles.
+ * <p>
+ * This solver is based on <a href="https://github.com/mxgmn/WaveFunctionCollapse">Maxim Gumin's work</a>
+ * on the Wave Function Collapse (WFC) algorithm. It applies WFC, a constraint satisfaction
+ * algorithm, to find a solution to a given Sudoku grid. The solver iteratively collapses cells
+ * with the minimum entropy until a solution is found or a contradiction is encountered,
+ * requiring a rollback.
+ */
 public class WaveFunctionCollapse extends Solver {
 
     private static final Logger logger = LoggerFactory.getLogger(
@@ -20,6 +29,12 @@ public class WaveFunctionCollapse extends Solver {
 
     int[][] entropy;
 
+    /**
+     * Constructs a {@code WaveFunctionCollapse} solver for the given Sudoku grid.
+     *
+     * @param grid The Sudoku grid to be solved.
+     * @see Solver#Solver(Grid)
+     */
     public WaveFunctionCollapse(Grid grid) {
         super(grid);
         int sizeX = grid.getSize().getX();
@@ -33,6 +48,12 @@ public class WaveFunctionCollapse extends Solver {
         );
     }
 
+    /**
+     * Initializes the entropy matrix for the Sudoku grid.
+     * <p>
+     * For each cell, if it's empty, the entropy is set to the number of possible values it can hold.
+     * If the cell is already filled, the entropy is set to -1.
+     */
     private void fillEntropy() {
         logger.debug(
             Colors.DEBUG_COLOR +
@@ -54,6 +75,16 @@ public class WaveFunctionCollapse extends Solver {
         }
     }
 
+    /**
+     * Rolls back the last move by resetting the symbol in the last inserted cell.
+     * <p>
+     * This method retrieves the last inserted position from {@code lastInserts}, resets the symbol
+     * in the grid at that position, and updates the history. It also propagates the entropy change
+     * caused by removing the symbol.
+     *
+     * @see Solver#rollBack()
+     * @see #propagateEntropy(String, Position, boolean)
+     */
     @Override
     protected void rollBack() {
         if (this.lastInserts.isEmpty()) {
@@ -79,12 +110,28 @@ public class WaveFunctionCollapse extends Solver {
         this.propagateEntropy(lastSymbolInserted, lastMovePosition, false);
     }
 
+    /**
+     * Inserts a symbol into the Sudoku grid at the specified position.
+     * <p>
+     * This method extends the base {@link Solver#insertSymbol(String, Position)} method by also
+     * propagating the entropy changes after inserting the symbol.
+     *
+     * @param symbol   The symbol to insert.
+     * @param position The position to insert the symbol at.
+     * @see Solver#insertSymbol(String, Position)
+     * @see #propagateEntropy(String, Position, boolean)
+     */
     @Override
     protected void insertSymbol(String symbol, Position position) {
         super.insertSymbol(symbol, position);
         this.propagateEntropy(symbol, position, true);
     }
 
+    /**
+     * Solves the Sudoku puzzle using the Wave Function Collapse algorithm.
+     * <p>
+     * The Wave Function Collapse algorithm iteratively reduces uncertainty in the grid until a solution is found.
+     */
     @Override
     public void solve() {
         this.lastInserts = new ArrayList<>();
@@ -177,6 +224,10 @@ public class WaveFunctionCollapse extends Solver {
         return solutions.size();
     }
 
+    /**
+     * Finds positions with the minimum entropy in the grid.
+     * <p>
+     */
     private Entropy getPositionsMinimumEntropy() {
         Entropy cellsEntropy = new Entropy();
         for (int y = 0; y < this.grid.getSize().getY(); y++) {
@@ -200,6 +251,9 @@ public class WaveFunctionCollapse extends Solver {
         return cellsEntropy;
     }
 
+    /**
+     * Propagates the entropy changes to related cells after inserting or removing a symbol.
+     */
     private void propagateEntropy(
         String symbol,
         Position position,
